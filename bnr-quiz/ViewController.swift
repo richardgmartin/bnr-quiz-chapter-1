3f7532ada1d5e6c9d2d9cad67cbd355aee0154f6
richardgmartin <richardgeorgemartin@gmail.com>
2016-02-28 16:32:16 -0600

    first commit

---
+++ bnr-quiz/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  bnr-quiz
+//
+//  Created by Richard Martin on 2016-02-28.
+//  Copyright © 2016 Richard Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+
+    
+    }
+
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+        ++currentQuestionIndex
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+        
+        
+    }
+    
+    let questions: [String] = ["From what is cognac made?",
+                                " What is 7 + 7",
+                                "What is capital of Vermont?"]
+    
+    let answers: [String] = ["Grapes",
+                            " 14",
+                            "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+
+
+}
+
